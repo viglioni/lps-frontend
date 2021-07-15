77d04a6b943587db019b24d7c758c147c3ae6423
Laura <laura.viglioni@loft.com.br>
2021-07-15 00:10:05 -0300

    feat: added album's cover to card

--- src/components/disc-card/index.tsx
+++ src/components/disc-card/index.tsx
@@ -1,49 +1,29 @@
-import { Card, CardContent, Grid, makeStyles } from '@material-ui/core'
-import { map } from 'ramda'
+import { Card, CardHeader, CardMedia, makeStyles } from '@material-ui/core'
 import React from 'react'
-import { toTypography } from '../../helpers/elements'
-import { convertToString, toDate } from '../../helpers/string'
 import { LP } from '../../shared/types/lps'
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 300,
-    maxWidth: 500,
     width: '30%',
+    minWidth: 300,
     margin: '1%',
   },
-  pos: {
-    marginBottom: 12,
+  media: {
+    height: 0,
+    paddingTop: '100%',
   },
 })
 
 export const DiscCard = (info: LP): JSX.Element => {
-  const { root } = useStyles()
-
-  const albumName = toTypography(info.name, { variant: 'h6', align: 'center' })
-  const artist = toTypography(info.artist, {
-    variant: 'body1',
-    align: 'center',
-  })
-  const [year, boughtat] = map(
-    text => toTypography(text, { variant: 'overline', align: 'center' }),
-    [convertToString(info.year), toDate(info.boughtat)],
-  )
+  const { root, media } = useStyles()
 
   return (
     <Card className={root} key={info.id}>
-      <CardContent>
-        <Grid container direction="column">
-          <Grid> {albumName}</Grid>
-          <Grid>{artist}</Grid>
-          <Grid>{year}</Grid>
-          <Grid container direction="row">
-            <Grid item xs={6}>
-              {boughtat}
-            </Grid>
-          </Grid>
-        </Grid>
-      </CardContent>
+      <CardMedia className={media} image={info.cover_url} />
+      <CardHeader
+        title={info.name}
+        subheader={info.artist + ', ' + info.year}
+      />
     </Card>
   )
 }
