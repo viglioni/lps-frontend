297b48e1060063e9db168adab7f3e24013991a77
Laura <laura.viglioni@loft.com.br>
2021-07-14 21:34:47 -0300

    feat: added loading and error msg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,38 @@
-import { Grid, makeStyles } from '@material-ui/core'
+import { CircularProgress, Grid, makeStyles } from '@material-ui/core'
 import React, { useEffect, useState } from 'react'
 import { getAllLPs } from '../api/getLPs'
 import { DiscCard } from '../components/disc-card'
+import { toTypography } from '../helpers/elements'
 import { mapToComponent } from '../helpers/functors'
 import { LPs } from '../shared/types/lps'
 
 const useStyles = makeStyles({
   root: {
     padding: '5%',
+    minHeight: '100vh',
   },
 })
 
 const Home = (): JSX.Element => {
   const [lps, setLps] = useState<LPs>([])
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const [hasError, setError] = useState<boolean>(false)
+
   const { root } = useStyles()
 
-  useEffect(() => {
+  useEffect((): void => {
+    setLoading(true)
     getAllLPs()
       .then(({ data }) => {
         setLps(data.LPs)
       })
-      .catch(console.error)
+      .catch(err => {
+        console.error(err)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
   return (
@@ -30,6 +42,9 @@ const Home = (): JSX.Element => {
       alignContent="space-around"
       justifyContent="space-evenly"
     >
+      {isLoading && <CircularProgress />}
+      {hasError &&
+        toTypography('Something went wrong. Please reload the page.')}
       {mapToComponent(lps, DiscCard)}
     </Grid>
   )
